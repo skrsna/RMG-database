a3be1af0bdeedfba5fb85ec692903ca63679a512
Alon Grinberg Dana <alongd@mit.edu>
2020-01-22 10:47:54 -0500

    f decreased rate rules of new fams other than aminal hydrolysis

--- input/kinetics/families/hemiacetal_hydrolysis/rules.py
+++ input/kinetics/families/hemiacetal_hydrolysis/rules.py
@@ -10,7 +10,7 @@ longDesc = u"""
 entry(
     index=0,
     label="hemiacetal",
-    kinetics=ArrheniusEP(A=(3e+12, 's^-1'), n=0, alpha=0, E0=(0, 'kcal/mol')),
+    kinetics=ArrheniusEP(A=(3e+10, 's^-1'), n=0, alpha=0, E0=(0, 'kcal/mol')),
     rank=0,
     shortDesc=u"""Default""",
 )
